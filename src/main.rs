f55943accaf7e601c5f333e98ebae97657a0d270
roottool <operation.versatile.roottool@gmail.com>
2022-09-21 10:42:29 +0000

    feat: Implement fizzbuzz for 1..100

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+mod fizzbuzz;
 mod std_in_num;
 
 fn main() {
-  let num = std_in_num::input_num();
-  println!("{}", num);
+    // let num = std_in_num::input_num();
+    // println!("{}", num);
+
+    fizzbuzz::range_fizzbuzz();
 }
